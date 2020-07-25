2d7ffcd64472e45c8c92d302063a01e3c47f7b7e
Vũ Huy Hiếu <62875324+v-hius@users.noreply.github.com>
2020-07-25 10:53:33 +0700

    update kml

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,91 +1,89 @@
-function initMap()
-{
-    mapOptions = {
-            center: { lat: 21.0717671, lng: 105.7740281 },
-            zoom: 10,
-            mapTypeId: 'terrain',
-            streetViewControl: false,
-            fullscreenControl: false,
-            mapTypeControlOptions: {
-                position: google.maps.ControlPosition.TOP_RIGHT
-            },
-            zoomControlOptions: {
-                position: google.maps.ControlPosition.RIGHT_BOTTOM
-            }
-    };
-    var map = new google.maps.Map(document.getElementById('content-wrapper'), mapOptions);
+function initMap() {
+  mapOptions = {
+          center: { lat: 21.0717671, lng: 105.7740281 },
+          zoom: 10,
+          mapTypeId: 'terrain',
+          streetViewControl: false,
+          fullscreenControl: false,
+          mapTypeControlOptions: {
+              position: google.maps.ControlPosition.TOP_RIGHT
+          },
+          zoomControlOptions: {
+              position: google.maps.ControlPosition.RIGHT_BOTTOM
+          }
+  };
+  var map = new google.maps.Map(document.getElementById('content-wrapper'), mapOptions);
 
-    var kmlLayer = new google.maps.KmlLayer({
-          url: "https://v-hius.github.io/assets/kml/kmlfile.kml",
-          suppressInfoWindows: true,
+  var kmlLayer = new google.maps.KmlLayer({
+        url: "https://v-hius.github.io/assets/kml/kmlfile.kml",
+        suppressInfoWindows: true,
+        map: map
+      });
+/*
+  var infowindow = new google.maps.InfoWindow({
+    content: "Test"
+  });
+*/
+  var myMarker = new google.maps.Marker(
+      {
+          animation: google.maps.Animation.DROP,
           map: map
-        });
-    kmlLayer.addListener('click', function(kmlEvent) {
-          var text = kmlEvent.featureData.description;
-          showInContentWindow(text);
-        });
-      
-    function showInContentWindow(text) {
-          var sidediv = document.getElementById('content-window');
-          sidediv.innerHTML = text;
-        }
+      });
+/*
+  kmlLayer.addListener('click', function() {
+    infowindow.open(map, myMarker);
+  });
+  */
+  //Your Location
+  addYourLocationButton(map, myMarker);
 
-    var myMarker = new google.maps.Marker(
-        {
-            animation: google.maps.Animation.DROP,
-            map: map
-        });
-
-    //Your Location
-    addYourLocationButton(map, myMarker);
-
-    //Search Place
-    //addSearchButton();
-    searchPlace(map);
+  //Search Place
+  //addSearchButton();
+  searchPlace(map);
 }
 
 function addYourLocationButton(map, marker) {
-    var controlDiv = document.createElement('div');
+  var controlDiv = document.createElement('div');
 
-    var ControlUI = document.createElement('button');
-    ControlUI.style.backgroundColor = '#fff';
-    ControlUI.style.border = 'none';
-    ControlUI.style.outline = 'none';
-    ControlUI.style.width = '40px';
-    ControlUI.style.height = '40px';
-    ControlUI.style.borderRadius = '2px';
-    ControlUI.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
-    ControlUI.style.marginLeft = '10px';
-    ControlUI.style.marginRight = '10px';
-    ControlUI.style.padding = '0px';
-    ControlUI.title = 'Vị trí của bạn';
-    controlDiv.appendChild(ControlUI);
+  var ControlUI = document.createElement('button');
+  ControlUI.style.backgroundColor = '#fff';
+  ControlUI.style.border = 'none';
+  ControlUI.style.outline = 'none';
+  ControlUI.style.width = '40px';
+  ControlUI.style.height = '40px';
+  ControlUI.style.borderRadius = '2px';
+  ControlUI.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+  ControlUI.style.marginLeft = '10px';
+  ControlUI.style.marginRight = '10px';
+  ControlUI.style.padding = '0px';
+  ControlUI.title = 'Vị trí của bạn';
+  controlDiv.appendChild(ControlUI);
 
-    var secondChild = document.createElement('div');
-    secondChild.style.margin = '4px';
-    secondChild.style.width = '32px';
-    secondChild.style.height = '32px';
-    secondChild.style.backgroundImage = 'url(https://v-hius.github.io/assets/img/mylocation.png)';
-    secondChild.style.backgroundRepeat = 'no-repeat';
-    secondChild.id = 'you_location_img';
+  var secondChild = document.createElement('div');
+  secondChild.style.margin = '4px';
+  secondChild.style.width = '32px';
+  secondChild.style.height = '32px';
+  secondChild.style.backgroundImage = 'url(https://v-hius.github.io/assets/img/mylocation.png)';
+  secondChild.style.backgroundRepeat = 'no-repeat';
+  secondChild.id = 'you_location_img';
 
-    ControlUI.appendChild(secondChild);
+  ControlUI.appendChild(secondChild);
 
-    ControlUI.addEventListener('click', function() {
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                marker.setPosition(latlng);
-                map.setCenter(latlng);
-            });
-        }
-        else {
-            $('#you_location_img').css('background-position', '0px 0px');
-        }
-    });
+  ControlUI.addEventListener('click', function() {
+      if(navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+              var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+              marker.setPosition(latlng);
+              map.setCenter(latlng);
+          });
+      }
+      else {
+          $('#you_location_img').css('background-position', '0px 0px');
+      }
+  });
 
-    controlDiv.index = 1;
-    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+  controlDiv.index = 1;
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
 }
 /*
 function addSearchButton() {
@@ -135,64 +133,64 @@ function addSearchButton() {
 }
 */
 function searchPlace(map) {
-    // Create the search box and link it to the UI element.
-    const input = document.getElementById("searchBox");
-    const searchBox = new google.maps.places.SearchBox(input);
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener("bounds_changed", () => {
-      searchBox.setBounds(map.getBounds());
-    });
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("searchBox");
+  const searchBox = new google.maps.places.SearchBox(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+  searchBox.setBounds(map.getBounds());
+  });
 
-    let markers = [];
+  let markers = [];
 
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener("places_changed", () => {
-      const places = searchBox.getPlaces();
-  
-      if (places.length == 0) {
-        return;
-      }
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+  const places = searchBox.getPlaces();
 
-      // Clear out the old markers.
-      markers.forEach(marker => {
-        marker.setMap(null);
-      });
-      markers = [];
+  if (places.length == 0) {
+    return;
+  }
 
-      // For each place, get the icon, name and location.
-      const bounds = new google.maps.LatLngBounds();
-      places.forEach(place => {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        
-        const icon = {
-          url: place.icon,
-          size: new google.maps.Size(80, 80),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-        
-        // Create a marker for each place.
-        markers.push(
-          new google.maps.Marker({
-            map,
-            icon,
-            title: place.name,
-            position: place.geometry.location
-          })
-        );
-  
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
+  // Clear out the old markers.
+  markers.forEach(marker => {
+    marker.setMap(null);
+  });
+  markers = [];
+
+  // For each place, get the icon, name and location.
+  const bounds = new google.maps.LatLngBounds();
+  places.forEach(place => {
+    if (!place.geometry) {
+      console.log("Returned place contains no geometry");
+      return;
+    }
+    
+    const icon = {
+      url: place.icon,
+      size: new google.maps.Size(80, 80),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    
+    // Create a marker for each place.
+    markers.push(
+      new google.maps.Marker({
+        map,
+        icon,
+        title: place.name,
+        position: place.geometry.location
+      })
+    );
+
+    if (place.geometry.viewport) {
+      // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    } else {
+      bounds.extend(place.geometry.location);
+    }
+  });
+  map.fitBounds(bounds);
+  });
 }
